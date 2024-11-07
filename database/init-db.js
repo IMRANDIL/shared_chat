72bc0e184e4d6a21e15e36653af8b55a2a7de41d
IMRANDIL <aliimranadil2@gmail.com>
2024-11-07 11:58:36 +0530

    more changes

--- database/init-db.js
+++ database/init-db.js
@@ -1,57 +1,58 @@
 // shared/config/database.js
-const { sq } = require('../config/connect');
-const {defineAssociations} = require('../models/model-index')
+const { sq } = require("../config/connect");
+const { defineAssociations } = require("../models/model-index");
 
 // Define retry configuration
 const MAX_RETRIES = 5;
 const RETRY_DELAY = 2000; // 2 seconds
 
-
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // Load models
-require('../models/model-index')
-
+require("../models/model-index");
 
 // Initialize database connection and synchronize models
 const initializeDatabase = async (retries = MAX_RETRIES) => {
   while (retries > 0) {
     try {
       await sq.authenticate();
-      console.log('Database connection established.');
+      console.log("Database connection established.");
 
       //define assoc
       defineAssociations();
 
       // Synchronize all defined models
-      await sq.sync({alter: false});
+      await sq.sync({ alter: false });
 
-      console.log('Database synchronized successfully.');
+      console.log("Database synchronized successfully.");
 
       // Handle disconnection events
-      sq.connectionManager.on('disconnection', async () => {
-        console.warn('Database connection lost. Reconnecting...');
+      // Handle connection events using the recommended approach
+      sq.addHook("afterDisconnect", async () => {
+        console.warn("Database connection lost. Reconnecting...");
         await initializeDatabase();
       });
-      sq.connectionManager.on('error', async (error) => {
-        console.error('Database error encountered:', error);
-        await initializeDatabase();
-      });
-
       return; // Exit once connected successfully
     } catch (error) {
-      console.error(`Database connection failed. Retries left: ${retries - 1}`, error);
+      console.error(
+        `Database connection failed. Retries left: ${retries - 1}`,
+        error
+      );
       retries -= 1;
 
       if (retries === 0) {
-        console.error('Max retries reached. Could not connect to the database.');
+        console.error(
+          "Max retries reached. Could not connect to the database."
+        );
         throw error;
       }
 
-      console.log(`Retrying database connection in ${RETRY_DELAY / 1000} seconds...`);
+      console.log(
+        `Retrying database connection in ${RETRY_DELAY / 1000} seconds...`
+      );
       await sleep(RETRY_DELAY);
     }
   }
 };
 
-module.exports = {  initializeDatabase };
+module.exports = { initializeDatabase };
